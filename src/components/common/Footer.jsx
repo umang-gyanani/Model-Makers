e1f4b2c17e06cfb1e90401be64524f13ebc42891
umang-gyanani <umagyanani80@gmail.com>
2025-06-03 16:25:42 +0530

    btn

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "./button";
+import Button from "./Button";
 import { FaceBookIcon, FooterLogo, InstaGramIcon, TwiterIcon } from "./Icon";
 
 function Footer() {
